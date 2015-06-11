673f6d866d293d1a18a628c65ab51108fd6b735a
Filiecs <Filiecs2@gmail.com>
2015-06-11 00:50:32 -0700

    Adding Space.h

---
+++ Source/Components/Systems/Space.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "SinEntity.h"
+#include "DataLevel.h"
+
+class Game;
+typedef Space ObjectSpace;
+
+class Space : public GameObject
+{
+public:
+    ZilchDeclareDerivedType(Space, GameObject);
+    Space() :GameObject(){}
+    Space(const String& name) : GameObject(name){}
+    
+    void InitializeSpace();
+
+    void OnObjectCreated(EventData* data);
+    void OnObjectDestroyed(EventData* data);
+
+    void LoadLevel(DataLevel* level);
+    
+    void UninitializeSpace();
+
+    const Array<Handle>& ObjectList = Children;
+
+    ~Space() {};
+private:
+    unsigned ObjectCount = 0;
+    
+};
